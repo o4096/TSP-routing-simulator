4f8e95712110c93fa1c7bcff706a8d75f4148e9b
OmarAtyaMahboub <OmarAtyaMahboub@gmail.com>
2025-04-19 17:36:53 +0200

    - new file HybridACO algorithim TBA in GUI  - new feature to add initial starting points (colored in orange)

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ class Node:
 		self.data=   None
 		self.radius= 10
 
-	def draw(self, canvas):
-		canvas.create_oval(self.x-self.radius, self.y-self.radius, self.x+self.radius, self.y+self.radius, fill=self.color)
+	def draw(self, canvas, color =None):
+		canvas.create_oval(self.x-self.radius, self.y-self.radius, self.x+self.radius, self.y+self.radius, fill=color if color else self.color)
 		canvas.create_text(self.x,             self.y, text=str(self.id), fill='black')
 
 class MainApp:
@@ -22,6 +22,7 @@ class MainApp:
 		self.root= root
 		self.root.title('Graph Drawing and TSP Solver')
 		self.selected_node= None
+		self.inital_nodes= []
 		
 		# Initialize graph
 		self.graph= nx.Graph()
@@ -50,16 +51,16 @@ class MainApp:
 	def mb_left(self, event):
 		node_hit= self._get_mouse_collision(event.x, event.y)
 		if node_hit:
-			check = True
-			if self.selected_node: #link nodes
+			if not self.selected_node: # no selected nodes
+				self.selected_node = node_hit
+			elif self.selected_node == node_hit and node_hit not in self.inital_nodes: #add to inital nodes
+				self.inital_nodes.append(node_hit)
+			else:#link nodes
 				if(self.weight_valid()):
 					self.graph.add_edge(self.selected_node, node_hit, weight = int(self.input_weight.get()))
 					print(nx.adjacency_matrix(self.graph,weight='weight'))
 				else:
 					messagebox.showerror('error','weight should be numbers!')
-					check = False
-					
-			if (check): self.selected_node= node_hit
 		else:
 			self.graph.add_node(Node(len(self.graph.nodes), event.x, event.y))
 		self.canvas_redraw()
@@ -70,6 +71,7 @@ class MainApp:
 			if node_hit is self.selected_node:
 				self.selected_node= None
 			else:
+				self.inital_nodes.remove(node_hit)
 				self.graph.remove_node(node_hit)
 		else:
 			self.selected_node= None
@@ -99,6 +101,7 @@ class MainApp:
 	def canvas_clear(self):
 		Node.obj_count = 0
 		self.selected_node = None
+		self.inital_nodes = []
 		self.canvas.delete('all')
 		self.graph.clear()
 
@@ -110,13 +113,13 @@ class MainApp:
 			self.canvas.create_text((n1.x+n2.x)/2 - 5, (n1.y+n2.y)/2 - 5, text=str(self.graph.edges[n1,n2]['weight']))
 
 		for node in self.graph.nodes:	#draw nodes
-			node.draw(self.canvas)
+			node.draw(self.canvas, 'orange' if node in self.inital_nodes else None)
 
 		# selection rendering is separate from all other node rendering to separate UI interaction from any graph animation
 		if self.selected_node:		#if there's selected node, highlight it
 			self.canvas.create_oval(self.selected_node.x-self.selected_node.radius, self.selected_node.y-self.selected_node.radius, #top left
 			   			self.selected_node.x+self.selected_node.radius, self.selected_node.y+self.selected_node.radius, #bottom right
-						outline='red', width=2, fill='grey')
+						outline='red', width=2, fill= 'orange' if (self.selected_node in self.inital_nodes) else 'grey')
 			self.canvas.create_text(self.selected_node.x,             self.selected_node.y, text=str(self.selected_node.id), fill='black')
 
 if __name__=='__main__':
