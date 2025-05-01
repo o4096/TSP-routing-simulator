6e49f7423e3f1ddca4e3fc6617c9e60f3ac406e0
OmarAtyaMahboub <OmarAtyaMahboub@gmail.com>
2025-05-01 20:35:12 +0300

    animation for each iteration

--- main.py
+++ main.py
@@ -40,6 +40,7 @@ class MainApp:
 		self.canvas.pack(side=LEFT, fill=BOTH, expand=True)
 		self.canvas.bind('<Button-1>', self.mb_left)
 		self.canvas.bind('<Button-3>', self.mb_right)
+		# self.bind('<KeyPress-H>', self.run) # DOES NOT WORK FOR SOME REASONE HAD THE RUN FUNCTION CALLED AT THE END OF INIT
 		self.canvas.grid_rowconfigure(100)
 		self.canvas.grid_columnconfigure(100)
 		self.canvas.grid(column=10, row=10)
@@ -61,6 +62,7 @@ class MainApp:
 		# self.textbox_seed= Entry(self.control_frame)
 		# self.textbox_seed.pack(pady=10)
 		self.canvas_redraw()
+		self.run()#TEMPORARY SOLUTION
 	
 	def reseed(self):
 		seed= self.textbox_seed.get()
@@ -123,21 +125,13 @@ class MainApp:
 				children_tours= generate_children(colony.get_best(10), num_children=10, mutation_rate=0.1)
 				colony.replace_worst(children_tours)
 			
-			#TODO: intuitive animation system
-
+			#animation system
 			best= colony.get_best()[0]
-			print(f'Iteration {iteration+1:2d}/{ITERATIONS} - Best Distance: {best.cost}')
-			loss[iteration]= best.cost
-
-		dt= time.time()-t0
-
-
-		#animation system
-		animation_delay = 0.05
-		for ant in colony.ants:
+			# best tour of each iteration
+			animation_delay = 0.05
 			lines = []
 			prev_i = None
-			for i in ant.tour:
+			for i in best.tour:
 				#node
 				self.nodes[i].color = 'orange'
 				#edge
@@ -145,33 +139,65 @@ class MainApp:
 					lines.append({"x0":self.nodes[prev_i].x,"y0":self.nodes[prev_i].y,"x1":self.nodes[i].x,"y1":self.nodes[i].y})	
 					#store node to be used as prev
 				prev_i = i
-
 				#redraw
 				self.canvas.delete('all')
 				for line in lines:
 					self.canvas.create_line(line["x0"], line["y0"], line["x1"], line["y1"], fill='orange', width=2)
 				for node in self.nodes:
 					node.draw(self.canvas)
-				self.canvas.update()
-				
+				self.canvas.update()		
 				#delay
 				time.sleep(animation_delay)
-			#clear for next ant tour
+			#clear for the next iteration
 			for node in self.nodes:
 				if node.color == "orange":
 					node.color="white" 
 			self.canvas_redraw()
+			print(f'Iteration {iteration+1:2d}/{ITERATIONS} - Best Distance: {best.cost}')
+			loss[iteration]= best.cost
+		dt= time.time()-t0
 
 
-		best= colony.get_best()[0]
-		print(f'Best Tour: {[self.nodes[i].id for i in range(len(best.tour))]}')
+		#animation system
+		# ant tours of final iteration
+		# animation_delay = 0.05
+		# for ant in colony.ants:
+		# 	lines = []
+		# 	prev_i = None
+		# 	for i in ant.tour:
+		# 		#node
+		# 		self.nodes[i].color = 'orange'
+		# 		#edge
+		# 		if prev_i != None:
+		# 			lines.append({"x0":self.nodes[prev_i].x,"y0":self.nodes[prev_i].y,"x1":self.nodes[i].x,"y1":self.nodes[i].y})	
+		# 			#store node to be used as prev
+		# 		prev_i = i
+		# 		#redraw
+		# 		self.canvas.delete('all')
+		# 		for line in lines:
+		# 			self.canvas.create_line(line["x0"], line["y0"], line["x1"], line["y1"], fill='orange', width=2)
+		# 		for node in self.nodes:
+		# 			node.draw(self.canvas)
+		# 		self.canvas.update()
+		# 		#delay
+		# 		time.sleep(animation_delay)
+		# 	#clear for next ant tour
+		# 	for node in self.nodes:
+		# 		if node.color == "orange":
+		# 			node.color="white" 
+		# 	self.canvas_redraw()
+		print(f'Best Tour: {[self.nodes[i].id for i in best.tour]}')
 		print(f'Best Distance: {best.cost} km')
 		print(f'Algorithm Time Taken: {dt} seconds')
 
 		#REDRAW
 		self.canvas.delete('all')
 		for i in range(len(best.tour)-1):
-			self.canvas.create_line(self.nodes[i].x, self.nodes[i].y, self.nodes[i+1].x, self.nodes[i+1].y, fill='red', width=2)
+			idx1 = best.tour[i]
+			idx2 = best.tour[i+1]
+			self.canvas.create_line(self.nodes[idx1].x, self.nodes[idx1].y, self.nodes[idx2].x, self.nodes[idx2].y, fill='red', width=2)
+		self.canvas.create_line(self.nodes[best.tour[-1]].x, self.nodes[best.tour[-1]].y,
+						   self.nodes[best.tour[0]].x, self.nodes[best.tour[0]].y, fill='red', width=2)
 		for node in self.nodes:
 			node.draw(self.canvas)
 
